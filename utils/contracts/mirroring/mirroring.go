5f128e89c828c654addc91627765799cebeffc57
Marko Boben <marko.boben@gmail.com>
2023-12-27 13:05:25 +0100

    Removed unused contracts

--- utils/contracts/mirroring/mirroring.go
+++
@@ -1,2 +0,0 @@
-//go:generate  abigen --abi=mirroring.abi --pkg=mirroring --type=Mirroring --out=autogen.go
-package mirroring
--- utils/contracts/voting/voting.go
+++
@@ -1,2 +0,0 @@
-//go:generate  abigen --abi=voting.abi --pkg=voting --type=Voting --out=autogen.go
-package voting
