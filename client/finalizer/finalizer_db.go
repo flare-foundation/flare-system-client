354de0f32b249d0dde07e55ccecc805ab4cc2c78
Tilen Marc <tilen.marc@abelium.com>
2024-09-19 09:35:00 +0200

    Renaming files.

---
+++ client/finalizer/finalizer_db.go
@@ -0,0 +1,43 @@
+package finalizer
+
+import (
+	"encoding/hex"
+	"flare-fsc/database"
+
+	"github.com/ethereum/go-ethereum/common"
+	"gorm.io/gorm"
+)
+
+type finalizerDB interface {
+	FetchTransactionsByAddressAndSelector(
+		address common.Address, selector []byte, from int64, to int64,
+	) ([]database.Transaction, error)
+	FetchTransactionsByAddressAndSelectorFromBlockNumber(
+		address common.Address, selector []byte, from int64,
+	) ([]database.Transaction, error)
+	FetchLogsByAddressAndTopic0(common.Address, string, int64, int64) ([]database.Log, error)
+}
+
+type finalizerDBImpl struct {
+	client *gorm.DB
+}
+
+func (db finalizerDBImpl) FetchTransactionsByAddressAndSelector(
+	address common.Address, selector []byte, from, to int64,
+) ([]database.Transaction, error) {
+	hexSelector := hex.EncodeToString(selector)
+	return database.FetchTransactionsByAddressAndSelector(db.client, address.Hex(), hexSelector, from, to)
+}
+
+func (db finalizerDBImpl) FetchTransactionsByAddressAndSelectorFromBlockNumber(
+	address common.Address, selector []byte, from int64,
+) ([]database.Transaction, error) {
+	hexSelector := hex.EncodeToString(selector)
+	return database.FetchTransactionsByAddressAndSelectorFromBlockNumber(db.client, address.Hex(), hexSelector, from)
+}
+
+func (db finalizerDBImpl) FetchLogsByAddressAndTopic0(
+	address common.Address, topic0 string, from, to int64,
+) ([]database.Log, error) {
+	return database.FetchLogsByAddressAndTopic0(db.client, address.Hex(), topic0, from, to)
+}
--- client/protocol/submitter.go
+++ client/protocol/submitter.go
@@ -11,7 +11,9 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/ava-labs/coreth/accounts"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/pkg/errors"
 )
@@ -182,5 +184,97 @@ func newSignatureSubmitter(
 		},
 		maxRounds:      submitCfg.MaxRounds,
 		messageChannel: messageChannel,
+	}
+}
+
+// WritePayloadV2 encodes payload to buffer.
+// Payload data should be valid (data length 38, additional data length <= maxuint16 - 66)
+func (s *SignatureSubmitter) WritePayloadV2(
+	buffer *bytes.Buffer, currentEpoch int64, data *SubProtocolResponse, protocolID uint8,
+) error {
+	dataHash := accounts.TextHash(crypto.Keccak256(data.Data))
+	signature, err := crypto.Sign(dataHash, s.protocolContext.signerPrivateKey)
+	if err != nil {
+		return errors.Wrap(err, "error signing submitSignatures data")
+	}
+
+	epochBytes := shared.Uint32toBytes(uint32(currentEpoch - 1))
+	lengthBytes := shared.Uint16toBytes(uint16(66 + len(data.AdditionalData)))
+
+	buffer.WriteByte(protocolID) // Protocol ID (1 byte)
+	buffer.Write(epochBytes[:])  // Epoch (4 bytes)
+	buffer.Write(lengthBytes[:]) // Length (2 bytes)
+	buffer.WriteByte(1)          // Type (1 byte)
+
+	buffer.WriteByte(signature[64] + 27) // V (1 byte)
+	buffer.Write(signature[0:32])        // R (32 bytes)
+	buffer.Write(signature[32:64])       // S (32 bytes)
+
+	buffer.Write(data.AdditionalData)
+	return nil
+}
+
+// RunEpochV2 gets the submitSignature messages from the subprotocols providers.
+//  1. run every sub-protocol provider with delay of 1 second at most five times.
+//  2. repeat 1 for each sub-protocol provider not giving valid answer.
+//
+// Repeat 1 and 2 until all sub-protocol providers give valid answer or we did maxRounds attempts.
+func (s *SignatureSubmitter) RunEpochV2(currentEpoch int64) {
+	logger.Info("Submitter %s running for epoch %d [%v, %v]", s.name, currentEpoch, s.epoch.StartTime(currentEpoch), s.epoch.EndTime(currentEpoch))
+
+	protocolsToSend := make(map[int]bool)
+	for i := range s.subProtocols {
+		protocolsToSend[i] = true
+	}
+	channels := make([]<-chan shared.ExecuteStatus[*SubProtocolResponse], len(s.subProtocols))
+	for i := 0; i < s.maxRounds && len(protocolsToSend) > 0; i++ {
+		for i, protocol := range s.subProtocols {
+			if !protocolsToSend[i] {
+				continue
+			}
+			channels[i] = protocol.getDataWithRetry(
+				currentEpoch-1,
+				"submitSignatures",
+				s.protocolContext.submitSignaturesAddress.Hex(),
+				s.dataFetchRetries,
+				s.dataFetchTimeout,
+				SignatureSubmitterDataVerifier,
+			)
+		}
+
+		protocolsSent := []int{}
+
+		buffer := bytes.NewBuffer(nil)
+		buffer.Write(s.selector)
+		for i := range s.subProtocols {
+			if !protocolsToSend[i] {
+				continue
+			}
+
+			data := <-channels[i]
+			if !data.Success {
+				logger.Error("Error getting data for submitter %s: %s", s.name, data.Message)
+				continue
+			}
+			err := s.WritePayloadV2(buffer, currentEpoch, data.Value, s.subProtocols[i].ID)
+			if err != nil {
+				logger.Error("Error writing payload for submitter %s: %v", s.name, err)
+				continue
+			}
+
+			// send message to finalizer
+			s.messageChannel <- shared.ProtocolMessage{ProtocolID: s.subProtocols[i].ID, VotingRoundID: uint32(currentEpoch - 1), Message: data.Value.Data}
+
+			protocolsSent = append(protocolsSent, i)
+		}
+		if len(protocolsSent) > 0 {
+			if s.submit(buffer.Bytes()) {
+				for _, i := range protocolsSent {
+					delete(protocolsToSend, i)
+				}
+			}
+		} else {
+			logger.Info("Submitter %s did not get any new data", s.name)
+		}
 	}
 }
