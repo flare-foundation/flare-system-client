05f5e60bb1c4924841de3969aefea1a2caa804f8
Gregor Podlogar <gregor.podlogar@flare.network>
2026-07-13 13:26:31 +0200

    feat: support for voter(Pre)Registration soft switch for flr and sgb
    - switch of contract addresses
    - technically abi of contract changes. But the only change is in the voterRegistered event which is not parsed in the process. The methods that are called remain unchanged.
    - change of the message for registration. Now chainID is included in the hash

--- client/epoch/registry_utils.go
+++ client/epoch/registry_utils.go
@@ -30,23 +30,64 @@ const (
 	chainIDCoston  = 16
 	chainIDCoston2 = 114
 
-	breakingEpochCoston = 5451
+	chainIDFlare    = 14
+	chainIDSongbird = 19
+
+	breakingEpochCoston   = 5451
+	breakingEpochFlare    = 417
+	breakingEpochSongbird = 417
 )
 
 const (
+	// coston
+
 	newRegistryCostonAddr = "0x42F4526BFC6f892DB515a832a52eFc9edFADf6c0"
 	oldRegistryCostonAddr = "0xB4B93a3A3ADa93a574E6efeb5f295bf882934cB6"
 
 	newPreRegistryCostonAddr = "0x4A5538e86bc09cc1b02BAE720CCc39548e10dB28"
 	oldPreRegistryCostonAddr = "0xF660984B8597e31437cA4b7dEa0A41677982563b"
+
+	// flare
+
+	newRegistryFlareAddr = "0xA480457953Af3583E54DCd630b219353B8FC9Af7"
+	oldRegistryFlareAddr = "0x2580101692366e2f331e891180d9ffdF861Fce83"
+
+	newPreRegistryFlareAddr = "0x76D49E62B07e52A13b7FBB4602eD942f812c87e2"
+	oldPreRegistryFlareAddr = "0xeFDBf6F31Aa46c62414Aee82aF43036d16885b48"
+
+	// songbird
+
+	newRegistrySongbirdAddr = "0xd23FAE88c09e6A77dD9eFcc29D6bBC55D2e74310"
+	oldRegistrySongbirdAddr = "0x31B9EC65C731c7D973a33Ef3FC83B653f540dC8D"
+
+	newPreRegistrySongbirdAddr = "0xD8957603dE539118898BA2C321a1001d062Be7Ae"
+	oldPreRegistrySongbirdAddr = "0x9Ba9A142FD5B2953667B03dB40D1d77c83F225a2"
 )
 
 var (
+	// coston
+
 	newRegistryCoston = common.HexToAddress(newRegistryCostonAddr)
 	oldRegistryCoston = common.HexToAddress(oldRegistryCostonAddr)
 
 	newPreRegistryCoston = common.HexToAddress(newPreRegistryCostonAddr)
 	oldPreRegistryCoston = common.HexToAddress(oldPreRegistryCostonAddr)
+
+	// flare
+
+	newRegistryFlare = common.HexToAddress(newRegistryFlareAddr)
+	oldRegistryFlare = common.HexToAddress(oldRegistryFlareAddr)
+
+	newPreRegistryFlare = common.HexToAddress(newPreRegistryFlareAddr)
+	oldPreRegistryFlare = common.HexToAddress(oldPreRegistryFlareAddr)
+
+	// songbird
+
+	newRegistrySongbird = common.HexToAddress(newRegistrySongbirdAddr)
+	oldRegistrySongbird = common.HexToAddress(oldRegistrySongbirdAddr)
+
+	newPreRegistrySongbird = common.HexToAddress(newPreRegistrySongbirdAddr)
+	oldPreRegistrySongbird = common.HexToAddress(oldPreRegistrySongbirdAddr)
 )
 
 var (
@@ -162,16 +203,62 @@ func NewRegistryContractClient(
 	}
 
 	var oldRegistryBinding *registry.Registry
-	if registryAddress == newRegistryCoston {
+	switch registryAddress {
+	case newRegistryCoston:
+		if chainID != chainIDCoston {
+			return nil, fmt.Errorf("new registry mismatch address from %s chainID: %d", "Coston", chainID)
+		}
+
 		oldRegistryBinding, err = registry.NewRegistry(oldRegistryCoston, ethClient)
+		if err != nil {
+			return nil, err
+		}
+	case newRegistryFlare:
+		if chainID != chainIDFlare {
+			return nil, fmt.Errorf("new registry mismatch address from %s chainID: %d", "Flare", chainID)
+		}
+
+		oldRegistryBinding, err = registry.NewRegistry(oldRegistryFlare, ethClient)
+		if err != nil {
+			return nil, err
+		}
+	case newRegistrySongbird:
+		if chainID != chainIDSongbird {
+			return nil, fmt.Errorf("new registry mismatch address from %s chainID: %d", "Songbird", chainID)
+		}
+
+		oldRegistryBinding, err = registry.NewRegistry(oldRegistrySongbird, ethClient)
 		if err != nil {
 			return nil, err
 		}
 	}
 
 	var oldPreRegistryBinding *preregistry.Preregistry
-	if preregistryAddress == newPreRegistryCoston {
+	switch preregistryAddress {
+	case newPreRegistryCoston:
+		if chainID != chainIDCoston {
+			return nil, fmt.Errorf("new pre registry mismatch address from %s chainID: %d", "Coston", chainID)
+		}
+
 		oldPreRegistryBinding, err = preregistry.NewPreregistry(oldPreRegistryCoston, ethClient)
+		if err != nil {
+			return nil, err
+		}
+	case newPreRegistryFlare:
+		if chainID != chainIDFlare {
+			return nil, fmt.Errorf("new pre registry mismatch address from %s chainID: %d", "Flare", chainID)
+		}
+
+		oldPreRegistryBinding, err = preregistry.NewPreregistry(oldPreRegistryFlare, ethClient)
+		if err != nil {
+			return nil, err
+		}
+	case newPreRegistrySongbird:
+		if chainID != chainIDSongbird {
+			return nil, fmt.Errorf("new pre registry mismatch address from %s chainID: %d", "Songbird", chainID)
+		}
+
+		oldPreRegistryBinding, err = preregistry.NewPreregistry(oldPreRegistrySongbird, ethClient)
 		if err != nil {
 			return nil, err
 		}
@@ -430,6 +517,12 @@ func shouldUseOldRegistry(epochID uint32, address common.Address) (bool, common.
 	if address == newRegistryCoston && epochID < breakingEpochCoston {
 		return true, oldRegistryCoston
 	}
+	if address == newRegistryFlare && epochID < breakingEpochFlare {
+		return true, oldRegistryFlare
+	}
+	if address == newRegistrySongbird && epochID < breakingEpochSongbird {
+		return true, oldRegistrySongbird
+	}
 
 	return false, address
 }
@@ -437,6 +530,12 @@ func shouldUseOldRegistry(epochID uint32, address common.Address) (bool, common.
 func shouldUseOldPreRegistry(epochID uint32, address common.Address) (bool, common.Address) {
 	if address == newPreRegistryCoston && epochID < breakingEpochCoston {
 		return true, oldPreRegistryCoston
+	}
+	if address == newPreRegistryFlare && epochID < breakingEpochFlare {
+		return true, oldPreRegistryFlare
+	}
+	if address == newPreRegistrySongbird && epochID < breakingEpochSongbird {
+		return true, oldPreRegistrySongbird
 	}
 
 	return false, address
@@ -449,7 +548,9 @@ func (r *registryContractClientImpl) signature(epochID uint32, address common.Ad
 	)
 	switch {
 	case r.chainID == chainIDCoston2,
-		r.chainID == chainIDCoston && epochID >= breakingEpochCoston:
+		r.chainID == chainIDCoston && epochID >= breakingEpochCoston,
+		r.chainID == chainIDFlare && epochID >= breakingEpochFlare,
+		r.chainID == chainIDSongbird && epochID >= breakingEpochSongbird:
 		signature, err = r.createSignatureNew(r.chainID, epochID, address)
 		if err != nil {
 			return nil, fmt.Errorf("creating new: %w", err)
